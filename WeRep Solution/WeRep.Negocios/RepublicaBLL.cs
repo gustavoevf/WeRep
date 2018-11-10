974d65ac37dfa902022e765d1002991b0680b070
gustavoevf <42592880+gustavoevf@users.noreply.github.com>
2018-11-10 00:36:29 -0200

    Início de desenvolvimento de Negócio

--- WeRep Solution/WeRep.Negocios/RepublicaBLL.cs
+++ WeRep Solution/WeRep.Negocios/RepublicaBLL.cs
@@ -1,10 +1,33 @@
-﻿namespace WeRep.Negocios
+﻿using WeRep.Models;
+//corrigir referencias 
+
+namespace WeRep.Negocios
 {
     public class RepublicaBLL
     {
-        void CadastrarRepublica()
+        void CadastrarRepublica(int id_adm, string rua, string bairro, int numero, string complemento, int vagas, bool adm_mora)
         {
-            
+            var NovaRepublica = new RepublicaModel();
+            NovaRepublica.id_adm = id_adm;
+            NovaRepublica.rua = rua;
+            NovaRepublica.bairro = bairro;
+            NovaRepublica.numero = numero;
+            NovaRepublica.complemento = complemento;
+            NovaRepublica.vagas = vagas;
+
+            // InserirRepublica(NovaRepublica);
+
+            if (adm_mora)
+            {
+              //  var UsuarioAdmin = MostrarUsuario(id_adm, true); //Implementar função no acesso 
+
+                var MoradorAdmin = new MoradorModel();
+              //  MoradorAdmin.id_rep = MostrarIdDaRepublica();
+              //  MoradorAdmin.id_user = UsuarioAdmin.id_user;
+
+              //  InserirMoradores(MoradorAdmin);
+            }
+                
         }
     }
 }
