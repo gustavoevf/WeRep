f3572886d4db0fb306b40deee5b59370768d6384
Gustavo Ferreira <gustavoevf@gmail.com>
2018-11-20 01:21:31 -0200

    Recursos Novos
    Inclusão da view parcial de kanban, adequações em todo o projeto

--- WeRep.Comum/Recursos.cs
+++ WeRep.Comum/Recursos.cs
@@ -5,10 +5,20 @@ using System.Web;
 
 namespace WeRep.Models
 {
-    public static class Recursos
+    public class Recursos
     {
         public enum tipoUsuario { Inicial = 1, Morador, Administrador };
         public enum corNome { Green = 1, Yellow, Red, Black, Blue, Pink };
         public enum erroListagem { UsuarioJaExistente = 1, UsuarioInexistente, SenhaErrada };
+
+        public static List<string> cores = new List<string>()
+        {
+            "Green",
+            "Yellow",
+            "Red",
+            "Black",
+            "Blue",
+            "Pink"
+        };    
     }
 }
---
+++ WeRep.Negocios/KanbanBLL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Acesso;
+using WeRep.Models;
+
+namespace WeRep.Negocios
+{
+    public class KanbanBLL
+    {
+        public void CriarKanban(KanbanModel novo_kanban)
+        {
+            new KanbanDAL().CriarKanban(novo_kanban);
+        }
+
+        public List<KanbanModel> RetornarKanban(int id_rep, int? cor)
+        {
+            return new KanbanDAL().RetornarKanban(id_rep, cor);
+        }
+
+        public List<KanbanModel> RetornarKanban(int id_rep, string texto, int? cor)
+        {
+            return new KanbanDAL().RetornarKanban(id_rep, texto, cor);
+        }
+    }
+}
