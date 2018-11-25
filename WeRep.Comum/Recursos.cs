16dc914aa53429b8dbf241fd1dc3825395e5ff21
Gustavo <gustavoevf@gmail.com>
2018-11-25 20:48:22 -0200

    Alterações na funcionalidade de Kanban
    -Não está inserindo mensagem
    -Criação de tela InserirKanban

--- WeRep.Comum/Recursos.cs
+++ WeRep.Comum/Recursos.cs
@@ -8,7 +8,7 @@ namespace WeRep.Models
     public class Recursos
     {
         public enum tipoUsuario { Inicial = 1, Morador, Administrador };
-        public enum corNome { Green = 1, Yellow, Red, Black, Blue, Pink };
+        public enum corNome { Green = 1, Yellow, Red, Blue, Pink };
         public enum erroListagem { UsuarioJaExistente = 1, UsuarioInexistente, SenhaErrada };
 
         public static List<string> cores = new List<string>()
@@ -16,7 +16,6 @@ namespace WeRep.Models
             "Green",
             "Yellow",
             "Red",
-            "Black",
             "Blue",
             "Pink"
         };    
--- WeRep.Externo/Controllers/KanbanController.cs
+++ WeRep.Externo/Controllers/KanbanController.cs
@@ -16,11 +16,42 @@ namespace WeRep.Controllers
             return kanbanBLL.RetornarKanban(id_rep, null);
         }
 
-        public ActionResult Index(int id)
+        public ActionResult InserirKanban(int id_rep)
         {
-            var kanban = RetornarKanban(id);
+            var session_rep = (int)Session["id_rep"];
+
+            return View(id_rep);
+        }
+
+        public ActionResult Index(int id_rep)
+        {
+            var kanban = RetornarKanban(id_rep);
 
             return View(kanban);
         }
+
+        [HttpPost]
+        public ActionResult InserirKanban(KanbanModel msg)
+        {
+            var session_rep = (int)Session["id_rep"];
+            msg.id_rep = session_rep;
+
+            return RedirectToAction("Index", "Kanban");
+        }
+
+        //private int CalcularCor(string cor)
+        //{
+        //    if (cor == "Green")
+        //        return 1;
+        //    if (cor == "Yellow")
+        //        return 2;
+        //    if (cor == "Red")
+        //        return 3;
+        //    if (cor == "Blue")
+        //        return 4;
+        //    if (cor == "Pink")
+        //        return 5;
+        //    else return 0;
+        //}
     }
 }
