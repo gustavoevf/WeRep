293fcf478dee709af2029b0196e7244b4ac20048
Gustavo <gustavoevf@gmail.com>
2018-11-25 23:55:05 -0200

    Commit final

--- WeRep.Testes/Class1.cs
+++ WeRep.Testes/Class1.cs
@@ -1,10 +1,44 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using WeRep.Models;
+using WeRep.Negocios;
+using WeRep.Testes;
+
 
 namespace WeRep.Testes
 {
-    class Class1
+    [TestClass]
+    class Testes
     {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var rep_bll = new RepublicaBLL();
+            var usr_bll = new UsuarioBLL();
+            var kan_bll = new KanbanBLL();
+        }
+
+        [TestMethod]
+        public void CriarUsuário()
+        {
+            var Usuario = new UsuarioModel();
+            Usuario.id_user = 1000;
+            Usuario.nome = "aaaaaa";
+            Usuario.senha = "aaaaaa";
+
+            Usuario.tipo = 1;
+            
+        }
+
+        [TestMethod]
+        public void CriarRepublica()
+        {
+            var Republica = new RepublicaModel();
+            Republica.id_adm = 10000;
+            Republica.id_rep = 10000;
+            Republica.capacidade = 100;
+        }
     }
 }
